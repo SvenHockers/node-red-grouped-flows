924ea4ba2e9d35ab4a51c00e7a8d9856b0eb96e2
Sven Hockers <90685770+SvenHockers@users.noreply.github.com>
2026-04-29 22:40:46 +0200

    Change permissions for grouped flows API endpoints
    Updated permission requirements for GET and POST requests to '/nr-grouped-flows/groups'.

--- grouped-flows.js
+++ grouped-flows.js
@@ -6,10 +6,14 @@ const path = require("path");
 module.exports = function(RED) {
     const groupsFilePath = path.join(RED.settings.userDir, "nr-grouped-flows-groups.json");
 
-    RED.httpAdmin.get("/nr-grouped-flows/groups", RED.auth.needsPermission(""), function(req, res) {
+    RED.httpAdmin.get("/nr-grouped-flows/groups", RED.auth.needsPermission("flows.read"), function(req, res) {
         fs.readFile(groupsFilePath, "utf8", function(err, data) {
             if (err) {
-                res.json({});
+                if (err.code === "ENOENT") {
+                    res.json({});
+                } else {
+                    res.status(500).json({ error: err.message });
+                }
                 return;
             }
             try {
@@ -20,7 +24,7 @@ module.exports = function(RED) {
         });
     });
 
-    RED.httpAdmin.post("/nr-grouped-flows/groups", RED.auth.needsPermission(""), function(req, res) {
+    RED.httpAdmin.post("/nr-grouped-flows/groups", RED.auth.needsPermission("flows.write"), function(req, res) {
         const data = req.body;
         if (!data || typeof data !== "object" || Array.isArray(data)) {
             res.status(400).json({ ok: false, error: "Invalid request body" });
